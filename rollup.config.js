cbb7cf59fc008f00f39c8859b252ace15eaa57a9
SideeID <sandipratama164026@gmail.com>
2025-04-10 11:20:42 +0700

    feat: update package and rollup configuration to include JSON plugin support

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 import dts from 'rollup-plugin-dts';
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 export default [
   {
@@ -23,6 +24,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
+      json(),
     ],
   },
   {
