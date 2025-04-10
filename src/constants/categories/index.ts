256461166e5b0c7e1b947d4afd67dc231b0b22cc
SideeID <sandipratama164026@gmail.com>
2025-04-10 14:53:56 +0700

    refactor categories index to enable category exports and improve code clarity

--- src/constants/categories/index.ts
+++ src/constants/categories/index.ts
@@ -1,31 +1,31 @@
-// import { sexual } from './sexual';
-// import { insult } from './insult';
+import { sexual } from './sexual';
+import { insult } from './insult';
 // import { profanity } from './profanity';
 // import { slur } from './slur';
 // import { drugs } from './drugs';
 // import { disgusting } from './disgusting';
 // import { blasphemy } from './blasphemy';
 
-// export const categories = {
-//   sexual, // Kata-kata berbau seksual
-//   insult, // Kata-kata penghinaan
-//   profanity, // Umpatan umum
-//   slur, // Perkataan merendahkan berdasarkan identitas
-//   drugs, // Terkait narkoba
-//   disgusting, // Kata-kata menjijikkan
-//   blasphemy, // Penistaan agama
-// };
+export const categories = {
+  sexual, // Kata-kata berbau seksual
+  insult, // Kata-kata penghinaan
+  // profanity, // Umpatan umum
+  // slur, // Perkataan merendahkan berdasarkan identitas
+  // drugs, // Terkait narkoba
+  // disgusting, // Kata-kata menjijikkan
+  // blasphemy, // Penistaan agama
+};
 
-// export { sexual, insult, profanity, slur, drugs, disgusting, blasphemy };
+export { sexual, insult };
 
-// export const allCategoryWords = [
-//   ...sexual,
-//   ...insult,
-//   ...profanity,
-//   ...slur,
-//   ...drugs,
-//   ...disgusting,
-//   ...blasphemy,
-// ];
+export const allCategoryWords = [
+  ...sexual,
+  ...insult,
+  // ...profanity,
+  // ...slur,
+  // ...drugs,
+  // ...disgusting,
+  // ...blasphemy,
+];
 
-// export default allCategoryWords;
+export default allCategoryWords;
