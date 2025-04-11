004a86b2b558ecf1e86b55d1326ea28541293960
SideeID <sandipratama164026@gmail.com>
2025-04-11 14:42:18 +0700

    refactor: standardize string quotes and formatting across profanity categories

--- src/constants/categories/blasphemy.ts
+++ src/constants/categories/blasphemy.ts
@@ -1,23 +1,23 @@
-import { ProfanityWord } from '../../types';
-import { general } from '../regions/general';
-import { jawa } from '../regions/jawa';
-import { sunda } from '../regions/sunda';
-import { betawi } from '../regions/betawi';
+import { ProfanityWord } from "../../types";
+import { general } from "../regions/general";
+import { jawa } from "../regions/jawa";
+import { sunda } from "../regions/sunda";
+import { betawi } from "../regions/betawi";
 
 export const blasphemy: ProfanityWord[] = [
-  ...general.filter((word) => word.category === 'blasphemy'),
-  ...jawa.filter((word) => word.category === 'blasphemy'),
-  ...sunda.filter((word) => word.category === 'blasphemy'),
-  ...betawi.filter((word) => word.category === 'blasphemy'),
+  ...general.filter((word) => word.category === "blasphemy"),
+  ...jawa.filter((word) => word.category === "blasphemy"),
+  ...sunda.filter((word) => word.category === "blasphemy"),
+  ...betawi.filter((word) => word.category === "blasphemy"),
 
   {
-    word: 'kafir',
-    category: 'blasphemy',
-    region: 'general',
+    word: "kafir",
+    category: "blasphemy",
+    region: "general",
     severity: 0.6,
-    aliases: ['kapir', 'kafur'],
-    description: 'Istilah untuk orang yang tidak percaya pada Islam',
-    context: 'Dapat bersifat merendahkan ketika digunakan terhadap non-Muslim',
+    aliases: ["kapir", "kafur"],
+    description: "Istilah untuk orang yang tidak percaya pada Islam",
+    context: "Dapat bersifat merendahkan ketika digunakan terhadap non-Muslim",
   },
 ];
 
--- src/constants/categories/drugs.ts
+++ src/constants/categories/drugs.ts
@@ -1,70 +1,70 @@
-import { ProfanityWord } from '../../types';
-import { general } from '../regions/general';
-import { jawa } from '../regions/jawa';
-import { sunda } from '../regions/sunda';
-import { betawi } from '../regions/betawi';
+import { ProfanityWord } from "../../types";
+import { general } from "../regions/general";
+import { jawa } from "../regions/jawa";
+import { sunda } from "../regions/sunda";
+import { betawi } from "../regions/betawi";
 
 export const drugs: ProfanityWord[] = [
-  ...general.filter((word) => word.category === 'drugs'),
-  ...jawa.filter((word) => word.category === 'drugs'),
-  ...sunda.filter((word) => word.category === 'drugs'),
-  ...betawi.filter((word) => word.category === 'drugs'),
+  ...general.filter((word) => word.category === "drugs"),
+  ...jawa.filter((word) => word.category === "drugs"),
+  ...sunda.filter((word) => word.category === "drugs"),
+  ...betawi.filter((word) => word.category === "drugs"),
 
   {
-    word: 'ganja',
-    category: 'drugs',
-    region: 'general',
+    word: "ganja",
+    category: "drugs",
+    region: "general",
     severity: 0.6,
-    aliases: ['cimeng', 'kanabis', 'marijuana'],
-    description: 'Tanaman yang mengandung zat psikoaktif',
-    context: 'Digunakan untuk merujuk pada narkotika jenis cannabis',
+    aliases: ["cimeng", "kanabis", "marijuana"],
+    description: "Tanaman yang mengandung zat psikoaktif",
+    context: "Digunakan untuk merujuk pada narkotika jenis cannabis",
   },
   {
-    word: 'sabu',
-    category: 'drugs',
-    region: 'general',
+    word: "sabu",
+    category: "drugs",
+    region: "general",
     severity: 0.8,
-    aliases: ['crystal', 'meth', 'ss'],
-    description: 'Narkotika jenis metamfetamin',
-    context: 'Istilah untuk metamfetamin yang merupakan narkotika berbahaya',
+    aliases: ["crystal", "meth", "ss"],
+    description: "Narkotika jenis metamfetamin",
+    context: "Istilah untuk metamfetamin yang merupakan narkotika berbahaya",
   },
   {
-    word: 'inex',
-    category: 'drugs',
-    region: 'general',
+    word: "inex",
+    category: "drugs",
+    region: "general",
     severity: 0.7,
-    aliases: ['ekstasi', 'pil'],
-    description: 'Obat terlarang yang mengandung MDMA',
-    context: 'Nama slang untuk obat terlarang ecstasy',
+    aliases: ["ekstasi", "pil"],
+    description: "Obat terlarang yang mengandung MDMA",
+    context: "Nama slang untuk obat terlarang ecstasy",
   },
   {
-    word: 'sakaw',
-    category: 'drugs',
-    region: 'general',
+    word: "sakaw",
+    category: "drugs",
+    region: "general",
     severity: 0.5,
-    aliases: ['ketagihan', 'menarik'],
-    description: 'Gejala putus obat pada pecandu',
+    aliases: ["ketagihan", "menarik"],
+    description: "Gejala putus obat pada pecandu",
     context:
-      'Menggambarkan kondisi pecandu yang sedang mengalami gejala putus obat',
+      "Menggambarkan kondisi pecandu yang sedang mengalami gejala putus obat",
   },
   {
-    word: 'ngepil',
-    category: 'drugs',
-    region: 'jawa',
+    word: "ngepil",
+    category: "drugs",
+    region: "jawa",
     severity: 0.3,
-    aliases: ['minum pil', 'telan pil'],
-    description: 'Mengkonsumsi obat-obatan terlarang dalam bentuk pil',
+    aliases: ["minum pil", "telan pil"],
+    description: "Mengkonsumsi obat-obatan terlarang dalam bentuk pil",
     context:
-      'Istilah dalam bahasa gaul untuk mengkonsumsi obat terlarang jenis tablet',
+      "Istilah dalam bahasa gaul untuk mengkonsumsi obat terlarang jenis tablet",
   },
   {
-    word: 'nyimeng',
-    category: 'drugs',
-    region: 'sunda',
+    word: "nyimeng",
+    category: "drugs",
+    region: "sunda",
     severity: 0.6,
-    aliases: ['nyimang', 'isap ganja'],
-    description: 'Mengisap ganja atau marijuana',
-    context: 'Istilah sunda untuk aktivitas mengkonsumsi ganja',
+    aliases: ["nyimang", "isap ganja"],
+    description: "Mengisap ganja atau marijuana",
+    context: "Istilah sunda untuk aktivitas mengkonsumsi ganja",
   },
 ];
 
