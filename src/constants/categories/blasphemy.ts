cc110b81130ad48b6e67a896884f2851d22afd6d
SideeID <sandipratama164026@gmail.com>
2025-04-13 09:04:16 +0700

    refactor: standardize import formatting and unify string quotation marks in blasphemy.ts

--- src/constants/categories/blasphemy.ts
+++ src/constants/categories/blasphemy.ts
@@ -1,25 +1,34 @@
-import { ProfanityWord } from "../../types";
-import { general } from "../regions/general";
-import { jawa } from "../regions/jawa";
-import { sunda } from "../regions/sunda";
-import { betawi } from "../regions/betawi";
-import {minang} from "../regions/minang";
+import { ProfanityWord } from '../../types';
+import { general } from '../regions/general';
+import { jawa } from '../regions/jawa';
+import { sunda } from '../regions/sunda';
+import { betawi } from '../regions/betawi';
+import { minang } from '../regions/minang';
+import madura from '../regions/madura';
+import aceh from '../regions/aceh';
+import bali from '../regions/bali';
+import batak from '../regions/batak';
 
 export const blasphemy: ProfanityWord[] = [
-  ...general.filter((word) => word.category === "blasphemy"),
-  ...jawa.filter((word) => word.category === "blasphemy"),
-  ...sunda.filter((word) => word.category === "blasphemy"),
-  ...betawi.filter((word) => word.category === "blasphemy"),
-  ...minang.filter((word)=>word.category==="blasphemy"),
+  ...general.filter((word) => word.category === 'blasphemy'),
+  ...jawa.filter((word) => word.category === 'blasphemy'),
+  ...sunda.filter((word) => word.category === 'blasphemy'),
+  ...betawi.filter((word) => word.category === 'blasphemy'),
+  ...minang.filter((word) => word.category === 'blasphemy'),
+  ...madura.filter((word) => word.category === 'blasphemy'),
+  ...sunda.filter((word) => word.category === 'blasphemy'),
+  ...aceh.filter((word) => word.category === 'blasphemy'),
+  ...bali.filter((word) => word.category === 'blasphemy'),
+  ...batak.filter((word) => word.category === 'blasphemy'),
 
   {
-    word: "kafir",
-    category: "blasphemy",
-    region: "general",
+    word: 'kafir',
+    category: 'blasphemy',
+    region: 'general',
     severity: 0.6,
-    aliases: ["kapir", "kafur"],
-    description: "Istilah untuk orang yang tidak percaya pada Islam",
-    context: "Dapat bersifat merendahkan ketika digunakan terhadap non-Muslim",
+    aliases: ['kapir', 'kafur'],
+    description: 'Istilah untuk orang yang tidak percaya pada Islam',
+    context: 'Dapat bersifat merendahkan ketika digunakan terhadap non-Muslim',
   },
 ];
 
