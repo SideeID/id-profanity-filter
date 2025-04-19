b9943cbf54aa94e53c708383900abf4af22eefd8
SideeID <sandipratama164026@gmail.com>
2025-04-19 22:59:28 +0700

    Add jest.setup.ts for global test configurations and mocking

---
+++ test/jest.setup.ts
@@ -0,0 +1,9 @@
+// Any global setup code can go here
+
+// For example, if you need to mock some global objects:
+// global.fetch = jest.fn();
+
+// Or silence console logs during tests
+// global.console.log = jest.fn();
+// global.console.warn = jest.fn();
+// global.console.error = jest.fn();
